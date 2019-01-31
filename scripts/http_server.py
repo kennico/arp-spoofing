b2645d03b44eed7b3a9a09bc1692c34312204def
ser_kenny <ser_kenny@outlook.com>
2019-01-31 20:40:27 +0800

    Add python scripts of a http server demo.

---
+++ scripts/http_server.py
@@ -0,0 +1,29 @@
+from http.server import SimpleHTTPRequestHandler
+from socketserver import ThreadingTCPServer
+
+page = """
+<html>
+<header><title>This is title</title></header>
+<body>
+Hello world
+</body>
+</html>
+"""
+
+
+class DemoHttpRequestHandler(SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Connection', 'close')
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(page.encode('utf-8'))
+
+
+if __name__ == '__main__':
+    try:
+        httpd = ThreadingTCPServer(('', 8000), DemoHttpRequestHandler)
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print('httpd exits.')
